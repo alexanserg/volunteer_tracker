93f12cd4d1eae63c1a08a98566cd78ae78904717
Student <example@example.com>
2019-08-04 20:14:46 -0700

    conitnue building project class as per specs

--- lib/project.rb
+++ lib/project.rb
@@ -9,4 +9,20 @@ class Project
     result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
     @id = result.first().fetch("id").to_i
   end
+
+  def ==(project)
+    @title == project.title
+  end
+
+  def self.all
+    returned = DB.exec("SELECT * FROM projects ORDER BY title;")
+    projects = []
+    returned.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
 end
