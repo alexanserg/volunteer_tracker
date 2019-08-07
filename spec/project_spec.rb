023e0c1be2029a4d9f24119c6955ea357e281a0c
Student <example@example.com>
2019-08-06 19:47:21 -0700

    change update test in project_spec to 'project.update('Teaching Ruby to Kids')' in order to fix bug

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -77,9 +77,7 @@ describe Project do
     it 'allows a user to update a project' do
       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
       project.save
-      binding.pry
-      project.update({:title => 'Teaching Ruby to Kids', :id => nil})
-      binding.pry
+      project.update('Teaching Ruby to Kids')
       expect(project.title).to eq 'Teaching Ruby to Kids'
     end
   end
